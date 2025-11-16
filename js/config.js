2703f164c425b02c0d030f0f46892fa7c59acdd9
PAT-07 <64210665+PAT-07@users.noreply.github.com>
2025-11-16 16:10:15 -0600

    config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_KcUVYHFP4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5l4okpujan252406qsfch9fqen', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
